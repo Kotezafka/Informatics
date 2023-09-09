328ee99bb116014f21ba183ac7224151c93ff34f
Kotezafka <samuseva.kate@yandex.ru>
2023-09-09 15:51:04 +0300

    theme4_d

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from theme4.whole_numbers.theme4_d import calculate_hours_and_minutes
 
 if __name__ == '__main__':
-    k, n = map(int, input().split())
+    d = int(input())
+
+    calculate_hours_and_minutes(d)
