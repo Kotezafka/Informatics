af0c9a34a7a6d3950a4e5b3e4a913a49cabf667e
Kotezafka <samuseva.kate@yandex.ru>
2023-10-04 15:21:55 +0300

    theme5_b

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from theme5.onedimensional_arrays.task_conditions_python.theme5_a import calculate_output_even_array_elements
+from theme5.onedimensional_arrays.task_conditions_python.theme5_b import calculate_output_even_array_elements
 
 if __name__ == '__main__':
     A = list(map(int, input().split()))
